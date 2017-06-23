60e8792e755284c8c7723db7c923f2b86e064b6b
zhangjunjun <google2720@aliyun.com>
2017-06-23 20:35:19 +0800

    1.drag Demo

--- base/src/main/java/com/view/SlideGroupHelper.java
+++ base/src/main/java/com/view/SlideGroupHelper.java
@@ -1,12 +1,20 @@
 package com.view;
 
+import android.animation.LayoutTransition;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Interpolator;
+import android.widget.ImageView;
 import android.widget.Scroller;
 
 import com.socks.library.KLog;
@@ -22,12 +30,13 @@ public class SlideGroupHelper {
     private SlideGroup slideGroup;
     private Scroller mScroller;
     private Context mContext;
-    private int downX,upX;
+    private int downX, upX;
     private GestureDetector mGestureDetector;
     private GestureListener mGestureListener;
-    private int mCurrentPage=0;
+    private int mCurrentPage = 0;
     private boolean isFling = false;
     private int mTouchSlop;
+    private LayoutTransition mLayoutTransition;
 
 
     private static class ScrollInterpolator implements Interpolator {
@@ -36,7 +45,7 @@ public class SlideGroupHelper {
 
         public float getInterpolation(float t) {
             t -= 1.0f;
-            return t*t*t*t*t + 1;
+            return t * t * t * t * t + 1;
         }
     }
 
@@ -44,41 +53,205 @@ public class SlideGroupHelper {
     public SlideGroupHelper(SlideGroup slide) {
         slideGroup = slide;
         mContext = slideGroup.getContext();
-        mScroller = new Scroller(mContext,new ScrollInterpolator());
+        mScroller = new Scroller(mContext, new ScrollInterpolator());
         slideGroup.setDisPathEvent(disPathEvent);
         mTouchSlop = ViewConfiguration.getTouchSlop();
         slideGroup.post(new Runnable() {
             @Override
             public void run() {
                 int childCount = slideGroup.getChildCount();
-                for (int i= 0;i<childCount;i++) {
-                   View view = slideGroup.getChildAt(i);
+                for (int i = 0; i < childCount; i++) {
+                    View view = slideGroup.getChildAt(i);
                     view.setClickable(true);
                     view.setOnClickListener(listener);
                     view.setLongClickable(true);
-                    view.setOnLongClickListener(longlistener);
+                   // view.setOnLongClickListener(longListener);
+                    setupDragSort(view);
                 }
+
+                mLayoutTransition = new LayoutTransition();
+                //去掉淡入淡出动画
+                mLayoutTransition.disableTransitionType(LayoutTransition.APPEARING);
+                mLayoutTransition.disableTransitionType(LayoutTransition.DISAPPEARING);
+                //add和remove的其余动画效果
+                // mLayoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+                //加入导致被影响的其余child做的动画，默认是移动，即被挤开。
+                mLayoutTransition.enableTransitionType(LayoutTransition.CHANGE_APPEARING);
+                mLayoutTransition.enableTransitionType(LayoutTransition.CHANGE_DISAPPEARING);
+                slideGroup.setLayoutTransition(mLayoutTransition);
             }
         });
+    }
+
+
+
+    public  void setupDragSort(View view) {
+        view.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(final View view, DragEvent event) {
+                ViewGroup viewGroup = (ViewGroup)view.getParent();
+                DragState dragState = (DragState)event.getLocalState();
+                switch (event.getAction()) {
+                    // 开始拖拽
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        if (view == dragState.view) {
+                            view.setVisibility(View.INVISIBLE);
+                        }
+                        break;
+                    // 结束拖拽
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        if (view == dragState.view) {
+                            view.setVisibility(View.VISIBLE);
+                        }
+                        break;
+
+                    // 拖拽进某个控件后，退出
+                    case DragEvent.ACTION_DRAG_EXITED:
+
+                        break;
+                    // 拖拽进某个控件后，保持
+                    case DragEvent.ACTION_DRAG_LOCATION: {
+                        if (view == dragState.view){
+                            break;
+                        }
+                        int index = viewGroup.indexOfChild(view);
+                        if (   (index > dragState.index && event.getY() > view.getHeight() / 2)
+                            || (index < dragState.index && event.getY() < view.getHeight() / 2)
+
+                            || (index < dragState.index && event.getX() < view.getWidth() / 2)
+                            || (index < dragState.index && event.getX() < view.getWidth() / 2)
+                                )
+                        {
+                            swapViews(viewGroup, view, index, dragState);
+                        } else {
+                            swapViewsBetweenIfNeeded(viewGroup, index, dragState);
+                        }
+                        break;
+                    }
+
+                    // 推拽进入某个控件
+                    case DragEvent.ACTION_DRAG_ENTERED:
+
+                        break;
+
+                    case DragEvent.ACTION_DROP:
+                        if (view != dragView) {
+                         //   swapViewGroupChildren(viewGroup, view, dragState.view);
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                    view.startDrag(null, new View.DragShadowBuilder(view), new DragState(view), 0);
+                return true;
+            }
+        });
+    }
+
+
+    private  void swapViewsBetweenIfNeeded(ViewGroup viewGroup, int index,
+                                                 DragState dragState) {
+        if (index - dragState.index > 1) {
+            int indexAbove = index - 1;
+            swapViews(viewGroup, viewGroup.getChildAt(indexAbove), indexAbove, dragState);
+        } else if (dragState.index - index > 1) {
+            int indexBelow = index + 1;
+            swapViews(viewGroup, viewGroup.getChildAt(indexBelow), indexBelow, dragState);
+        }
+    }
+
+
+    private  void swapViews(ViewGroup viewGroup, final View view, int index,
+                                  DragState dragState) {
+        swapViewsBetweenIfNeeded(viewGroup, index, dragState);
+        swapViewGroupChildren(viewGroup, view, dragState.view);
+        dragState.index = index;
+    }
+
+
+    public  void swapViewGroupChildren(ViewGroup viewGroup, View firstView, View secondView) {
+
+        int firstIndex = viewGroup.indexOfChild(firstView);
+        int secondIndex = viewGroup.indexOfChild(secondView);
+        if (firstIndex < secondIndex) {
+            viewGroup.removeViewAt(secondIndex);
+            viewGroup.removeViewAt(firstIndex);
+            viewGroup.addView(secondView, firstIndex);
+            viewGroup.addView(firstView, secondIndex);
+        } else {
+            viewGroup.removeViewAt(firstIndex);
+            viewGroup.removeViewAt(secondIndex);
+            viewGroup.addView(firstView, secondIndex);
+            viewGroup.addView(secondView, firstIndex);
+        }
 
 
     }
 
 
+    private  class DragState {
+        public View view;
+        public int index;
+        private DragState(View view) {
+            this.view = view;
+            index = ((ViewGroup)view.getParent()).indexOfChild(view);
+        }
+    }
+
+
+
+
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            KLog.d("tag :" +v.getTag());
+            KLog.d("tag :" + v.getTag());
         }
     };
 
-    private View.OnLongClickListener longlistener = new View.OnLongClickListener() {
+
+    private View.OnLongClickListener longListener = new View.OnLongClickListener() {
         @Override
         public boolean onLongClick(View v) {
-            KLog.d("onLongClick tag :" +v.getTag());
+            KLog.d("onLongClick tag :" + v.getTag());
+            KLog.d("long view  x : " + v.getX() + " --" + " y : " + v.getY());
+            handleDragDown(v);
             return true;
         }
     };
+
+
+
+    private void handleDragDown(View v) {
+        isDrag = true;
+        dragView = v;
+        //开启mDragItemView绘图缓存
+        dragView.setDrawingCacheEnabled(true);
+        //获取mDragItemView在缓存中的Bitmap对象
+        Bitmap mDragBitmap = Bitmap.createBitmap(dragView.getDrawingCache());
+        //释放绘图缓存，避免出现重复的镜像
+        dragView.destroyDrawingCache();
+        createDragImage(mDragBitmap, (int) dragView.getX(), (int) dragView.getY());
+        dragView.setVisibility(View.INVISIBLE);
+    }
+
+    public void handleDragUp(){
+        dragView.setVisibility(View.VISIBLE);
+        removeDragImage();
+        isDrag = false;
+    }
+
+
+    public void handleDragMove(int moveX, int moveY) {
+        if(dragView!=null) {
+            mWindowLayoutParams.x = moveX;
+            mWindowLayoutParams.y = moveY;
+            mWindowManager.updateViewLayout(mDragImageView, mWindowLayoutParams);
+        }
+    }
 
 
 
@@ -111,12 +284,12 @@ public class SlideGroupHelper {
                     else {
                         isIntercept = false;
                     }
-
                     break;
 
                 case MotionEvent.ACTION_CANCEL:
                 case MotionEvent.ACTION_UP:
                     isIntercept = false;
+                   // handleDragUp();
                     break;
 
             }
@@ -166,7 +339,11 @@ public class SlideGroupHelper {
 
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            scrollBy((int) distanceX);
+            if(!isDrag) {
+                scrollBy((int) distanceX);
+            }else {
+                handleDragMove((int)e2.getX(),(int) e2.getY());
+            }
             return false;
         }
 
@@ -177,7 +354,6 @@ public class SlideGroupHelper {
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
             //快速滑动时直接下/上一页
-           // Log("onFling  velocityX :" + velocityX);
             if(Math.abs(velocityX)>1500) {
                 isFling = true;
                 if (e1 != null && e2 != null) {
@@ -255,6 +431,8 @@ public class SlideGroupHelper {
         postInvalidate();
     }
 
+
+
     private void moveCurr(int page) {
         moveCurr(page,0);
     }
@@ -290,6 +468,53 @@ public class SlideGroupHelper {
         return slideGroup.getScrollX();
     }
 
+
+
+
+
+
+
+    private boolean isDrag;
+    private View dragView;
+    private WindowManager.LayoutParams mWindowLayoutParams;
+    private ImageView mDragImageView;
+    private WindowManager mWindowManager;
+
+    /**
+     * 创建拖动的镜像
+     * @param bitmap
+     * @param downX  按下的点相对父控件的X坐标
+     * @param downY  按下的点相对父控件的X坐标
+     */
+    private void createDragImage(Bitmap bitmap, int downX, int downY) {
+        if(mWindowLayoutParams==null) {
+            mWindowLayoutParams = new WindowManager.LayoutParams();
+        }
+        mWindowLayoutParams.format = PixelFormat.TRANSLUCENT; //图片之外的其他地方透明
+        mWindowLayoutParams.gravity = Gravity.TOP | Gravity.LEFT;
+        mWindowLayoutParams.x = downX;
+        mWindowLayoutParams.y = downY;
+        mWindowLayoutParams.alpha = 0.65f; //透明度
+        mWindowLayoutParams.width = bitmap.getWidth();
+        mWindowLayoutParams.height = bitmap.getHeight();
+        mWindowLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
+
+        mDragImageView = new ImageView(mContext);
+        mDragImageView.setImageBitmap(bitmap);
+        if(mWindowManager==null) {
+            mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        }
+        mWindowManager.addView(mDragImageView, mWindowLayoutParams);
+    }
+
+
+    private void removeDragImage() {
+        if (mDragImageView != null) {
+            mWindowManager.removeView(mDragImageView);
+            mDragImageView = null;
+        }
+    }
 
     private void Log(String info) {
         if(BuildConfig.DEBUG) {
---
+++ dragdemo/src/main/java/yunovo/com/dragdemo/MainActivity.java
@@ -0,0 +1,103 @@
+package yunovo.com.dragdemo;
+
+import android.content.ClipData;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnLongClickListener;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "DragDemo";
+    private ImageView image;
+    private ImageView image2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.image);
+        image2 = (ImageView) findViewById(R.id.image2);
+        image.setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // 创建DragShadowBuilder，我把控件本身传进去
+                DragShadowBuilder builder = new DragShadowBuilder(v);
+                // 剪切板数据，可以在DragEvent.ACTION_DROP方法的时候获取。
+                ClipData data = ClipData.newPlainText("dot", "Dot : " + v.toString());
+                // 开始拖拽
+                v.startDrag(data, builder, v, 0);
+                return true;
+            }
+        });
+        image.setOnDragListener(new OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                final int action = event.getAction();
+                switch (action) {
+                    // 开始拖拽
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        image.setVisibility(View.INVISIBLE);
+                        Log.d(TAG, "image1 ACTION_DRAG_STARTED");
+                        break;
+                    // 结束拖拽
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        image.setVisibility(View.VISIBLE);
+                        Log.d(TAG, "image1 ACTION_DRAG_ENDED");
+                        break;
+                    // 拖拽进某个控件后，退出
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        Log.d(TAG, "image1 ACTION_DRAG_EXITED");
+                        break;
+                    // 拖拽进某个控件后，保持
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        Log.d(TAG, "image1 ACTION_DRAG_LOCATION");
+                        break;
+                    // 推拽进入某个控件
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        Log.d(TAG, "image1 ACTION_DRAG_ENTERED");
+                        break;
+                    // 推拽进入某个控件，后在该控件内，释放。即把推拽控件放入另一个控件
+                    case DragEvent.ACTION_DROP:
+                        Log.d(TAG, "image1 ACTION_DROP");
+                        break;
+                }
+                return true;
+            }
+        });
+        image2.setOnDragListener(new OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                final int action = event.getAction();
+                switch (action) {
+                    case DragEvent.ACTION_DRAG_STARTED:
+                        Log.d(TAG, "image2 ACTION_DRAG_STARTED");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENDED:
+                        image2.setBackgroundColor(Color.TRANSPARENT);
+                        Log.d(TAG, "image2 ACTION_DRAG_ENDED");
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        image2.setBackgroundColor(Color.TRANSPARENT);
+                        Log.d(TAG, "image2 ACTION_DRAG_EXITED");
+                        break;
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        image2.setBackgroundColor(Color.BLUE);
+                        Log.d(TAG, "image2 ACTION_DRAG_ENTERED");
+                        break;
+                    case DragEvent.ACTION_DRAG_LOCATION:
+                        Log.d(TAG, "image2 ACTION_DRAG_LOCATION");
+                    case DragEvent.ACTION_DROP:
+                        Log.d(TAG, "image2 ACTION_DROP");
+                }
+                return true;
+            }
+        });
+    }
+
+}
